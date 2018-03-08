e0ed11a7df3efaa17a3a4e215457609974c9b7f8
ikbale <ikbale94@gmail.com>
2018-03-08 10:08:13 +0100

    add last 2 sections

--- scribbler.js
+++ scribbler.js
@@ -113,6 +113,9 @@ if (btns.length && sections.length > 0) {
   btns[3].addEventListener('click', function (event) {
     smoothScrollTo(sections[3], event);
   });
+   btns[4].addEventListener('click', function (event) {
+    smoothScrollTo(sections[4], event);
+  });
 }
 
 // fix menu to page-top once user starts scrolling
